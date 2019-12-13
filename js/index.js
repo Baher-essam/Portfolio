c070f7b6970cd2674c368608f7467143c2d765e7
Baher-essam <44957599+Baher-essam@users.noreply.github.com>
2019-12-13 17:41:36 +0200

    First upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+
+
+new fullpage('#fullpage',{
+  autoscrokking: true,
+  navigation : true,
+  anchors:[ 'section1' , 'section2' , 'section3' , 'section4' ],
+  navigationTooltips: ['Home' , 'About us' , 'Works' , 'Contact'],
+  showActiveTooltip: true,
+  scrollingSpeed: 1000,
+  controlArrows:false,
+  slidesNavigation: true
+
+})
