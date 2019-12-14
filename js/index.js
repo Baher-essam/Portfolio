5ab23dadff507437233e9c3fbdeac1e20faeb04a
Baher-essam <44957599+Baher-essam@users.noreply.github.com>
2019-12-14 07:59:43 +0200

    update 2

--- js/index.js
+++ js/index.js
@@ -4,11 +4,14 @@
 new fullpage('#fullpage',{
   autoscrokking: true,
   navigation : true,
-  anchors:[ 'section1' , 'section2' , 'section3' , 'section4' ],
+  anchors:[  'section1' , 'section2' , 'section3' , 'section4' ],
   navigationTooltips: ['Home' , 'About us' , 'Works' , 'Contact'],
   showActiveTooltip: true,
-  scrollingSpeed: 1000,
+  scrollingSpeed: 650,
   controlArrows:false,
-  slidesNavigation: true
-
+  slidesNavigation: true,
+  scrollBar: false,
+  parallax: true,
+  parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+  lazyLoading: true
 })
