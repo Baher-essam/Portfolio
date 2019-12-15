6c5dbe072cb913e7e0f58cb9c842a548c902254c
Baher-essam <44957599+Baher-essam@users.noreply.github.com>
2019-12-15 18:25:25 +0200

    particles js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 
 
 
+  //waiting screen for loading site
+  $(document).ready(function(){
+    $("#loading").fadeOut(2000);
+    $(".sk-cube-grid").fadeOut(2000 , function(){
+        $("html").css("overflow" , "auto");
+    })
+})
+  
+
+//fullpage plugsin
 new fullpage('#fullpage',{
   autoscrokking: true,
   navigation : true,
