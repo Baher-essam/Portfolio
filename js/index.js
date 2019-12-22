54daeea3587402a9f8e66389269b7b5442deee6c
Baher-essam <44957599+Baher-essam@users.noreply.github.com>
2019-12-22 02:40:13 +0200

    Removing fullpage js

--- js/index.js
+++ js/index.js
@@ -8,24 +8,6 @@
         $("html").css("overflow" , "auto");
     })
 })
-  
-
-//fullpage plugsin
-new fullpage('#fullpage',{
-  autoscrokking: true,
-  navigation : true,
-  anchors:[  'Header' , 'AboutMe' , 'Skills' , 'Portfolio' , 'Contact' ],
-  navigationTooltips: ['Header' , 'About Me' , 'Skills' , 'Portfolio' , 'Contact'],
-  showActiveTooltip: true,
-  scrollingSpeed: 650,
-  controlArrows:false,
-  slidesNavigation: true,
-  scrollBar: false,
-  parallax: true,
-  parallaxOptions: {type: 'reavel', percentage: 80, property: 'translate'},
-  lazyLoading: true
-})
-
 
 //wow.js plugsin
 new WOW().init();
