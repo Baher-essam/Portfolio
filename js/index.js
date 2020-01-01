24cfd8650a21f317314679a3fbca08b43191e85e
Baher-essam <44957599+Baher-essam@users.noreply.github.com>
2020-01-01 07:47:54 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -124,12 +124,12 @@ particlesJS('particles-js',
 
 
 
-  //waiting screen for loading site
-  $(document).ready(function(){
-    $("#loading").fadeOut(2000);
-    $(".sk-cube-grid").fadeOut(2000 , function(){
-        $("html").css("overflow" , "auto");
-    })
+//waiting screen for loading site
+$(document).ready(function(){
+  $("#loading").fadeOut(2000);
+  $(".sk-cube-grid").fadeOut(2000 , function(){
+      $("html").css("overflow" , "auto");
+  })
 })
 
 //wow.js fire function
